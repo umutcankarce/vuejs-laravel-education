b288cbbf122f7428b1ca7a90a56c9c4dd78eb8cc
Umut Can Karçe <umutcankarce1@gmail.com>
2022-10-09 22:06:35 +0300

    laravel-vuejs-app

---
+++ resources/js/sources/icons.js
@@ -0,0 +1,18 @@
+// core import function
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+// icons libs
+import {
+    faPlus,
+    faCircle,
+    faCaravan,
+    faHome,
+    faCoffee,
+    faCheck
+}
+from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+// icons add
+library.add(faPlus,faCheck, faEnvelope, faCircle, faCoffee ,faCaravan, faHome, faTwitter);
