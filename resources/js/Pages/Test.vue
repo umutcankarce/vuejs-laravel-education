18bbf305b3ec74e15ea32eacd4780eb0a6b5f462
Umut Can Karçe <umutcankarce1@gmail.com>
2022-11-01 19:13:52 +0200

    :fire: dark mode vueuse.org.

--- resources/js/Pages/Test.vue
+++ resources/js/Pages/Test.vue
@@ -1,14 +1,29 @@
 <script setup>
+import { useDark, useToggle } from '@vueuse/core'
 
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
 </script>
-
 
 <template>
 
-   <button class="btn btn-solid bg-pink">
-      Yeni Ürün Ekle
+   <div class="
+  max-w-[15rem]
+   mx-auto
+   p-10
+   flex
+   flex-grow
+   bg-slate-200
+   dark:bg-slate-700
+   dark:text-slate-100
+   ">
+   <span>Dark Mode</span>
+
+   <button @click="toggleDark()">
+      <font-awesome-icon :icon="'fa-solid '+ (isDark ? 'fa-sun' : 'fa-moon') " />
    </button>
- 
+      
+   </div>
 
 </template>
 
--- resources/js/sources/icons.js
+++ resources/js/sources/icons.js
@@ -8,11 +8,13 @@ import {
     faCaravan,
     faHome,
     faCoffee,
-    faCheck
+    faCheck,
+    faMoon,
+    faSun,
 }
 from '@fortawesome/free-solid-svg-icons';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 // icons add
-library.add(faPlus,faCheck, faEnvelope, faCircle, faCoffee ,faCaravan, faHome, faTwitter);
+library.add(faPlus, faCheck, faEnvelope, faCircle, faMoon, faSun, faCoffee, faCaravan, faHome, faTwitter);
