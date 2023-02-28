1d554f04e3c21fb11df42b01b2270811aa458d42
Umut Can Karçe <umutcankarce1@gmail.com>
2023-02-28 22:26:43 +0300

    radio button ve checbox button

---
+++ resources/js/Components/TRadioInput.vue
@@ -0,0 +1,62 @@
+
+<script setup>
+const props = defineProps({
+    label : String,
+    errors : [Object,Array],
+    modelValue : [Array,Object],
+    icon : String,
+    valueKey : {
+        type : String,
+        default : "id", 
+    },
+    labelKey : {
+        type : String,
+        default : "label", 
+    },
+    options: [Array,Object],
+    clearable : Boolean
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+const addValue = (value) => {
+   
+    let i = props.modelValue
+    if(!i.includes(value)){
+    i.push(value);
+    }else{
+       i.splice(i.findIndex(item => item === value));
+    }
+   emit('update:modelValue',i);
+}
+</script>
+
+<template>
+    <div class="flex flex-col flex-shrink-0 w-full">
+    <!-- Label -->
+     <span class="text-sm font-semibold">{{ label }}</span>
+        <div class="relative items-center flex space-x-2 min-h-[2.5rem]">
+        <!-- Icon -->
+        <font-awesome-icon v-if="icon" :icon="icon" class="absolute left-3 top-3" />
+            <!-- Input -->
+            <template v-for="i in options">
+                <input type="radio" 
+                :value="modelValue"
+             class="w-5 h-5 rounded-md" 
+             :class="icon ? 'pl-10' : ''" @change="addValue(i[valueKey])" :name="i[valueKey]">
+             <!-- Label -->
+             <label>{{ i[labelKey] }}</label>
+            </template>
+             <!-- Clear BTN -->
+             <div v-if="modelValue && clearable" class="absolute right-3 top-2.5 cursor-pointer" @click="$emit('update:modelValue','')">
+                x
+             </div>
+        </div>
+        <!-- Error -->
+        <div v-if="errors">
+            <template v-for="i in errors">
+                <span class="text-xs text-rose-600 dark:text-rose-400">{{ i }}</span>
+            </template>
+        </div>
+    </div>
+</template>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -5,6 +5,8 @@
     import TButton from "@/Components/TButton.vue";
     import TForm from "@/Components/TForm.vue";
     import TTextInput from "@/Components/TTextInput.vue";
+    import TCheckboxInput from "@/Components/TCheckboxInput.vue";
+    import TRadioInput from "@/Components/TRadioInput.vue";
     import TSelectInput from "@/Components/TSelectInput.vue";
 
 
@@ -27,11 +29,52 @@
         }
     ]
 
+    const SoftwareTools = [
+        {
+            id: "html",
+            label : "HTML5"
+        },
+        {
+            id: "css",
+            label : "CSS3"
+        },
+        {
+            id: "laravel",
+            label : "Laravel"
+        },
+        {
+            id: "vue",
+            label : "Vue"
+        }
+    ];
+
+    const jobType = [
+        {
+            id: "full-time",
+            label : "Tam Zamanlı"
+        },
+        {
+            id: "part-time",
+            label : "Yarı Zamanlı"
+        },
+        {
+            id: "intern",
+            label : "Stajyer"
+        },
+        {
+            id: "freelance",
+            label : "Freelance"
+        }
+    ]
+
     const form = ref({
         name : "",
         email : "",
         city : null,
+        softwareTools : []
     });
+
+  
 
     const handleSubmit = () => {
         alert("Form Gönderildi.");
@@ -44,14 +87,21 @@
 </script>
 <template>
     <VisitorLayout header="Form Component">
+        {{  form }}
         <t-form @submitted="handleSubmit" @reset="handleReset" title="Personel Ekle" subTitle="Personel İşlemleri için Kullanabilirsiniz.">
             <!-- Full Name -->
           <t-text-input label="Ad Soyad" class="col-span-3" icon="fa-solid fa-home" v-model="form.name" clearable />
           <!-- Email  -->
           <t-text-input label="E-Mail Adresi" class="col-span-3" v-model="form.email" :errors="['Bu Alan Zorunludur.']" clearable />
          
-          <!-- Şehir -->
+          <!-- City -->
           <t-select-input placeholder="Lütfen Seçim Yapınız." label="Şehir" class="col-span-3" clearable v-model="form.city" :options="options" labelKey="name" />
+          <!-- Space -->
+          <div class="col-span-3"></div>
+          <!-- Checkbox -->
+          <t-checkbox-input v-model="form.softwareTools" label="Yazılım Teknolojileri" class="col-span-6" :options="SoftwareTools" />
+          <!-- Radio -->
+          <t-radio-input :options="jobType" />
         </t-form>
     </VisitorLayout>
 </template>
